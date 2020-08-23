fb4a88f3fc5960a189c401beaf70831c7de9fad8
e-leal <leal.elyssa03@gmail.com>
2020-08-23 16:55:58 -0500

    added upvote functionality and conditional of appearing if logged in

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,14 +87,20 @@ router.post('/', (req, res) => {
 
       // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-        // create the vote
-        // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    console.log("user session id is: ", req.session.user_id);
+
+    Post.upvote(
+      { ...req.body, user_id: req.session.user_id },
+       { Vote, Comment, User }
+    )
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
         console.log(err);
-        res.status(400).json(err);
-    });
+        res.status(500).json(err);
+      });
+  }
 });
 
 router.put('/:id', (req, res) => {
